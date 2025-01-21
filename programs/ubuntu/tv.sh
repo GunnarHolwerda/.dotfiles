abb1bfa8876561bfb0d7f7b4fd43b6121e82d31c
Gunnar Holwerda <gunnarholwerda@gmail.com>
2025-01-20 18:21:05 -0800

    feat: add television to the setup

---
+++ programs/ubuntu/tv.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+VER=`curl -s "https://api.github.com/repos/alexpasmantier/television/releases/latest" | grep '"tag_name":' | sed -E 's/.*"tag_name": "([^"]+)".*/\1/'`
+curl -LO https://github.com/alexpasmantier/television/releases/download/$VER/television_$VER-1_amd64.deb
+echo $VER
+sudo dpkg -i television_$VER-1_amd64.deb
