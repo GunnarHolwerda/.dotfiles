f6539b10146430b0f10cfc9eeaa77b4dd08682d6
Gunnar Holwerda <gunnarholwerda@gmail.com>
2025-07-21 07:32:39 -0700

    feat: additional configuration

--- config/.config/nvim/lua/gunnar/remap.lua
+++ config/.config/nvim/lua/gunnar/remap.lua
@@ -4,5 +4,5 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Back to directory/tree view
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
-
-
+-- Neotree toggle
+vim.keymap.set('n', '<C-\\>', ':Neotree toggle<CR>')
